ce15b0302835b0668f5ad4ed43516281ba5d608e
Everaert Robin <r-everaert@outlook.fr>
2024-10-16 00:44:56 +0200

    posts added to store - getPosts() developped

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  userReducer  from "../feature/user.slice";
+import  userReducer from "../feature/user.slice";
+import  postsReducer from "../feature/posts.slice";
 
 export default configureStore({
     reducer: {
         user: userReducer,
+        posts: postsReducer
     },
 });
--- frontend/src/components/Thread.js
+++ frontend/src/components/Thread.js
@@ -1,22 +1,26 @@
 import { useEffect, useState } from "react"
 import axios from "axios";
 import { Post } from "./Post";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts } from "../feature/posts.slice";
 
 
 const Thread = () => {
 
-    const [posts, setPosts] = useState([]);
+    const dispatch = useDispatch();
+    const posts = useSelector(state => state.posts.postsData.message)
+
     useEffect(() => {
         axios.get("http://localhost:8000/post")
         .then(res => {
-            console.log(res.data.message)
-            setPosts(res.data.message);
+            dispatch(getPosts(res.data));
         })
     }, []);
   return (
     <div className="thread-container">
         <div className="post-container">
          {posts
+         .slice() // Creating an empty slice allows sorting when the data comes from RTK.
          .sort((a, b) => b.createdAt.localeCompare(a.createdAt))
          .map((post, index) => (
            <Post key={ post._id } post={ post } />
